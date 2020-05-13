9667f3a3c76b2033c538a65c43a846b7bebdddcd
Stephen Peters <7speter@gmail.com>
2020-05-13 17:12:01 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Person
+  def initialize(breed)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
+class Dog
+  def initialize(name)
+    @name =name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+      @name
+  end
+end
